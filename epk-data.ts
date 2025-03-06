957e808daa3c16644d50e79f8ed8d53695051b58
Eduardox3703 <eduardoxcelaya@gmail.com>
2025-03-06 10:20:25 -0600

    Primer commit

---
+++ epk-data.ts
@@ -0,0 +1,139 @@
+export const EPK_DATA = {
+    // "audios-promocionales": [
+    //     {
+    //         title: "Te Va Doler",
+    //         resources: [
+    //             {
+    //                 name: "Te_Va_A_Doler_Mastered.wav",
+    //                 link: "https://drive.google.com/file/d/1OEntjqMG_kZ4-ot0yaFKUsDymo2uu_4j/view?usp=sharing",
+    //             },
+    //         ],
+    //     },
+    //     {
+    //         title: "Mi Persona Preferida",
+    //         resources: [
+    //             {
+    //                 name: "Mi_Persona_Preferida_Mastered.wav",
+    //                 link: "https://drive.google.com/file/d/1EkPAHqI9cY7_hN7mO6sa5cEQdhg0sUPh/view?usp=sharing",
+    //             },
+    //         ],
+    //     },
+    //     {
+    //         title: "Tal Vez",
+    //         resources: [
+    //             {
+    //                 name: "Tal_Vez_Master_Mastered.wav",
+    //                 link: "https://drive.google.com/file/d/1QlulPx24e-VAYoCkvI3gtmCOdQO6sXyE/view?usp=sharing",
+    //             },
+    //         ],
+    //     },
+    // ],
+    "fotos-oficiales": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "FRAN FOTO 01.jpg",
+                    link: "https://drive.google.com/file/d/1qCuWUBT6ZnpJC0ojSoyJ2RbPoEweaKId/view?usp=sharing"
+                    
+                },
+                {
+                    name: "FRAN FOTO 02.jpg",
+                    link: "https://drive.google.com/file/d/1hMj2DICtX4N0adLJcUDwaHFeK4Mvs0Lg/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 03.jpg",
+                    link: "https://drive.google.com/file/d/1thCeYBB08xmdMjzYyiP6zUrfdj6qaLlu/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 04.jpg",
+                    link: "https://drive.google.com/file/d/1PmBAb35o0aFnUQpuFzrU193WVuAOYJH0/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 05.jpg",
+                    link: "https://drive.google.com/file/d/1ivON1bXj0NiWMZC09MXgbRZ6beI0I0iJ/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 06.jpg",
+                    link: "https://drive.google.com/file/d/19P6IjD4TpSTeABDvjhNoBZjq5Gj1MVte/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 07.jpg",
+                    link: "https://drive.google.com/file/d/1JL0nC8eHuJmuLnZbJ4SW7pvioO6qB3-Z/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 08.jpg",
+                    link: "https://drive.google.com/file/d/1JrLhpeu2yeNXN9LwZt7ISyoJ28OHdmOl/view?usp=sharing"
+               
+                }
+            ],
+        },
+    ],
+    "logotipo-oficial": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.png",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc",
+                },
+            ],
+        },
+    ],
+    "rider-tecnico": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.pdf",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc"
+                }
+            ]
+        }
+    ],
+    "brochure": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.pdf",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc"
+                }
+            ]
+        }
+    ],
+    biografia: [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.pdf",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc",
+                },
+            ],
+        },
+    ],
+    "videos-oficiales": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.mp4",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc",
+                },
+            ],
+        },
+    ],
+};
+
+export const EPK_VIEWS_NAMES: Record<string, string> = {
+    // "audios-promocionales": "Audios Promocionales",
+    "fotos-oficiales": "Fotos Oficiales",
+    "logotipo-oficial": "Logotipo Oficial",
+    "videos-oficiales": "Videos Oficiales",
+    // "temas-promocionales": "Temas Promocionales",
+    "rider-tecnico": "Rider Técnico",
+    "brochure": "Brochure",
+    biografia: "Biografía",
+    // "otros-elementos": "Otros Elementos",
+};
